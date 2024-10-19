55834fee19e47e489345eb827146e2977c6a4685
yanglee2421 <yanglee2421@gmail.com>
2024-10-19 10:14:40 +0800

    fix: catchError type

--- src/utils/catchError.ts
+++ src/utils/catchError.ts
@@ -1,15 +1,21 @@
 /**
  * Simplify catch error for promise
- * @param promise a promise
+ * @param promise a promise with data
+ * @param errMsg a string as error message if throwed is not a error
  * @returns a tuple, first element is the error last element is the data
  */
 export async function catchError<TData>(
-  promise: Promise<TData>
-): Promise<readonly [undefined, TData] | [unknown]> {
+  promise: Promise<TData>,
+  errMsg = ""
+): Promise<readonly [Error, undefined] | readonly [undefined, TData]> {
   try {
     const data = await promise;
     return [void 0, data] as const;
   } catch (error) {
-    return [error] as const;
+    if (error instanceof Error) {
+      return [error, void 0] as const;
+    }
+
+    return [new Error(errMsg, { cause: error }), void 0] as const;
   }
 }
